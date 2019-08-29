11f81470a211b90c2bd4312835540a0e3ad6a646
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-29 15:05:07 +0700

    update

--- DOM Challenge Starting Files/index.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('button').style.color = 'red';
-var liArr = document.querySelectorAll('li');
-// document.querySelectorAll('li a')[0].style.color = 'green';
-for (var i = 0; i < liArr.length; i++) {
-    liArr[i].style.color = 'green';
-}
-document.querySelector('h1').classList.toggle('h1Fontsize');
-document.querySelector('button').textContent = 'Pencet Aku';
