1fa310bf0b77b7c738ac8ec98bfaf0ce660a89cd
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-29 10:37:04 +0700

    update

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -1,1 +1,6 @@
-document.query
+document.querySelector('button').style.color = 'red';
+var liArr = document.querySelectorAll('li');
+// document.querySelectorAll('li a')[0].style.color = 'green';
+for (var i = 0; i < liArr.length; i++) {
+    liArr[i].style.color = 'green';
+}
